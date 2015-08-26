674d910019c974442270c44b382f108acf16ae92
Rob Kiefer <rob.kiefer@gmail.com>
2015-08-26 15:11:24 -0400

    Updates tests with non-deprecated constructor.

--- src/test/java/com/iobeam/api/resource/DeviceTest.java
+++ src/test/java/com/iobeam/api/resource/DeviceTest.java
@@ -13,13 +13,15 @@ import static org.junit.Assert.assertTrue;
 
 public class DeviceTest {
 
-    private static final String TEST_DEVICE_ID = "test1234only5678";
+    private static final String DEVICE_ID = "test1234only5678";
+    private static final String DEVICE_NAME = "java_test_ex";
+    private static final String DEVICE_TYPE = "java_test";
     private static final String TEST_DATE_STRING = "2015-03-01 20:55:21 -0400";
 
     private static final String jsonDevice = "{\n"
-                                             + "     \"device_id\": \"" + TEST_DEVICE_ID + "\",\n"
+                                             + "     \"device_id\": \"" + DEVICE_ID + "\",\n"
                                              + "     \"project_id\": 1000,\n"
-                                             + "     \"device_name\": \"java_test_ex\",\n"
+                                             + "     \"device_name\": \"" + DEVICE_NAME + "\",\n"
                                              + "     \"device_type\": \"java_test\",\n"
                                              + "     \"created\": \"" + TEST_DATE_STRING + "\"\n"
                                              + "}";
@@ -28,27 +30,42 @@ public class DeviceTest {
     public void testFromJson() throws Exception {
         Device d = Device.fromJson(new JSONObject(jsonDevice));
         assertNotNull(d);
-        assertEquals(TEST_DEVICE_ID, d.getId());
+        assertEquals(DEVICE_ID, d.getId());
         assertEquals(1000, d.getProjectId());
-        assertEquals("java_test_ex", d.getName());
-        assertEquals("java_test", d.getType());
+        assertEquals(DEVICE_NAME, d.getName());
+        assertEquals(DEVICE_TYPE, d.getType());
         Date expected = Util.DATE_FORMAT.parse(TEST_DATE_STRING);
         assertEquals(expected, d.getCreated());
+    }
+
+    private void verifyJson(JSONObject json, long pid, Date date) throws Exception {
+        assertNotNull(json);
+        assertEquals(DEVICE_ID, json.get("device_id"));
+        assertEquals(pid, json.getLong("project_id"));
+        assertEquals(DEVICE_NAME, json.get("device_name"));
+        assertEquals(DEVICE_TYPE, json.get("device_type"));
+        assertEquals(date, Util.DATE_FORMAT.parse(json.getString("created")));
     }
 
     @Test
     public void testSerialization() throws Exception {
         ResourceMapper mapper = new ResourceMapper();
         Date date = Util.DATE_FORMAT.parse(TEST_DATE_STRING);
-        Device d = new Device(TEST_DEVICE_ID, 1000, "java_test_ex", "java_test", date);
+        Device d = new Device(1000, new Device.Spec(DEVICE_ID, DEVICE_NAME, DEVICE_TYPE), date);
         byte[] raw = mapper.toJsonBytes(d);
         assertTrue(raw.length > 0);
         JSONObject json = new JSONObject(new String(raw, "UTF-8"));
-        assertNotNull(json);
-        assertEquals(TEST_DEVICE_ID, json.get("device_id"));
-        assertEquals(1000, json.get("project_id"));
-        assertEquals("java_test_ex", json.get("device_name"));
-        assertEquals("java_test", json.get("device_type"));
-        assertEquals(date, Util.DATE_FORMAT.parse(json.getString("created")));
+        verifyJson(json, 1000, date);
+    }
+
+    @Test
+    public void testDeprecatedSerialization() throws Exception {
+        ResourceMapper mapper = new ResourceMapper();
+        Date date = Util.DATE_FORMAT.parse(TEST_DATE_STRING);
+        Device d = new Device(DEVICE_ID, 1000, DEVICE_NAME, DEVICE_TYPE, date);
+        byte[] raw = mapper.toJsonBytes(d);
+        assertTrue(raw.length > 0);
+        JSONObject json = new JSONObject(new String(raw, "UTF-8"));
+        verifyJson(json, 1000, date);
     }
 }
