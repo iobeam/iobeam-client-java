364f333fe1f6b75fca2f45553eeeef23bcce6327
Rob Kiefer <rob.kiefer@gmail.com>
2015-11-23 17:39:20 -0500

    Add DataCallback as alias to SendCallback. SendCallback should replace DataCallback, but this allows users to get a deprecated notice.

---
+++ src/main/java/com/iobeam/api/client/DataCallback.java
@@ -0,0 +1,9 @@
+package com.iobeam.api.client;
+
+/**
+ * Please use the newer `SendCallback`. This will be removed in a later version.
+ */
+@Deprecated
+public abstract class DataCallback extends SendCallback {
+
+}
