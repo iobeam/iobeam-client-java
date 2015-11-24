70beaa94141bdce9f131438aa61fd6e2c1638086
Rob Kiefer <rob.kiefer@gmail.com>
2015-11-24 16:29:16 -0500

    Add support and tests for DataBatch

---
+++ src/main/java/com/iobeam/api/resource/DataBatch.java
@@ -0,0 +1,166 @@
+package com.iobeam.api.resource;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.logging.Logger;
+
+/**
+ * Represents a batch of data streams in column format. That is, columns are names for
+ * series/streams, and each row is a timestamp plus values for (some of) the series/streams.
+ *
+ * When converted to JSON, a "time" column is prepended to the other columns which are listed
+ * in alphabetical order. Rows are represented as a list with the first value being the timestamp,
+ * followed by corresponding values for each column. If a row does not have a value for a column,
+ * then it is filled in with null.
+ */
+public class DataBatch implements Serializable {
+
+    public static final class MismatchedLengthException extends RuntimeException {
+
+        public MismatchedLengthException() {
+            super("Number of columns and values are not the same.");
+        }
+    }
+
+    public static final class UnknownFieldException extends RuntimeException {
+
+        public UnknownFieldException(String field) {
+            super("Unknown field '" + field + "' trying to be added.");
+        }
+    }
+
+    private static final Logger logger = Logger.getLogger(DataBatch.class.getName());
+    private static final String KEY_COLUMNS = "fields";
+    private static final String KEY_ROWS = "data";
+
+
+    private final TreeSet<String> columns;
+    private final Map<Long, Map<String, Object>> rows = new TreeMap<Long, Map<String, Object>>();
+
+    /**
+     * Constructs a DataBatch, using the list to construct a _set_ of columns.
+     * Note: Duplicates will be removed and a warning will be logged.
+     *
+     * @param columns Set of field names to track in this batch.
+     */
+    public DataBatch(Set<String> columns) {
+        this.columns = new TreeSet<String>(columns);
+    }
+
+    /**
+     * Constructs a DataBatch, using the list to construct a _set_ of columns.
+     * Note: Duplicates will be removed and a warning will be logged.
+     *
+     * @param columns List of field names to track in this batch.
+     */
+    public DataBatch(List<String> columns) {
+        this.columns = new TreeSet<String>(columns);
+        if (columns.size() != this.columns.size()) {
+            logger.warning("Size mismatch in provided list of columns and resulting set of columns;" +
+                           " list may have contained duplicates.");
+        }
+    }
+
+    /**
+     * Add a data row to the batch at a particular timestamp.
+     *
+     * This method will throw a `MismatchedLengthException` if the length of `columns` and
+     * `values` are not the same.
+     *
+     * This method will throw an `UnknownFieldException` if `data` contains a key that is
+     * not in the set of columns this batch was constructed with.
+     *
+     * @param timestamp Timestamp for all data points
+     * @param columns The list of columns for a field to value mapping
+     * @param values The list of values for a field to value mapping
+     */
+    public void add(long timestamp, String[] columns, Object[] values) {
+        if (columns.length != values.length) {
+            throw new MismatchedLengthException();
+        }
+        Map<String, Object> temp = new HashMap<String, Object>();
+        for (int i = 0; i < columns.length; i++) {
+            temp.put(columns[i], values[i]);
+        }
+        add(timestamp, temp);
+    }
+
+    /**
+     * Add a data row to the batch at a particular timestamp.
+     *
+     * This method will throw an `UnknownFieldException` if `data` contains a key that is
+     * not in the set of columns this batch was constructed with.
+     *
+     * @param timestamp Timestamp for all data points
+     * @param data Map that has field names as keys and the data value as values.
+     */
+    public void add(long timestamp, Map<String, Object> data) {
+        for (String k : data.keySet()) {
+            if (!columns.contains(k)) {
+                throw new UnknownFieldException(k);
+            }
+        }
+        this.rows.put(timestamp, new HashMap<String, Object>(data));
+    }
+
+    /**
+     * Return a list of the field names tracked by this batch.
+     *
+     * @return List that is a copy of the columns tracked by this batch.
+     */
+    public List<String> getColumns() {
+        return new ArrayList<String>(this.columns);
+    }
+
+    // NOTE(robatticus): Not sure this will ever be needed, since batches are only sent TO iobeam,
+    // not received from iobeam.
+    public static DataBatch fromJson(final JSONObject json) throws ParseException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Convert this batch into a JSON representation.
+     *
+     * @return JSONObject representing this batch.
+     */
+    public JSONObject toJson() {
+        JSONObject ret = new JSONObject();
+        JSONArray columns = new JSONArray(new String[]{"time"});
+        for (String f : this.columns) {
+            columns.put(f);
+        }
+        ret.put(KEY_COLUMNS, columns);
+
+        JSONArray data = new JSONArray();
+        ret.put(KEY_ROWS, data);
+        for (Long ts : rows.keySet()) {
+            JSONArray row = new JSONArray();
+            row.put(ts);
+            Map<String, Object> temp = rows.get(ts);
+            for (String f : this.columns) {
+                row.put(temp.get(f));
+            }
+            data.put(row);
+        }
+
+        return ret;
+    }
+
+    @Override
+    public String toString() {
+        return "DataBatch{" +
+               "columns=" + this.columns +
+               "dataSize=" + this.rows.size() + 
+               "}";
+    }
+}
---
+++ src/test/java/com/iobeam/api/resource/DataBatchTest.java
@@ -0,0 +1,122 @@
+package com.iobeam.api.resource;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+public class DataBatchTest {
+
+    @Test
+    public void testDefensiveConstructorSet() throws Exception {
+        Set<String> fields = new HashSet<String>();
+        fields.add("a");
+        fields.add("b");
+        fields.add("c");
+        DataBatch batch = new DataBatch(fields);
+        List<String> batchFields = batch.getColumns();
+        assertEquals(fields.size(), batchFields.size());
+        fields.remove("a");
+        assertEquals(fields.size() + 1, batch.getColumns().size());
+    }
+
+    @Test
+    public void testDefensiveConstructorList() throws Exception {
+        List<String> fields = new ArrayList<String>();
+        fields.add("a");
+        fields.add("b");
+        fields.add("c");
+        DataBatch batch = new DataBatch(fields);
+        List<String> batchFields = batch.getColumns();
+        assertEquals(fields.size(), batchFields.size());
+        for (int i = 0; i < fields.size(); i++) {
+            assertEquals(fields.get(i), batchFields.get(i));
+        }
+        fields.set(0, "aa");
+        assertEquals("a", batch.getColumns().get(0));
+    }
+
+    @Test
+    public void testDefensiveGetterFields() throws Exception {
+        Set<String> fields = new HashSet<String>();
+        fields.add("a");
+        fields.add("b");
+        fields.add("c");
+        DataBatch batch = new DataBatch(fields);
+        List<String> batchFields = batch.getColumns();
+        batchFields.set(0, "aa");
+        assertEquals("a", batch.getColumns().get(0));
+    }
+
+    @Test
+    public void testToJson() throws Exception {
+        Set<String> fields = new HashSet<String>();
+        fields.add("b");
+        fields.add("a");
+        DataBatch batch = new DataBatch(fields);
+
+        List<Object[]> want = new ArrayList<Object[]>();
+        Map<String, Object> row = new HashMap<String, Object>();
+        row.put("a", 100);
+        row.put("b", 200);
+        batch.add(0, row);
+        want.add(new Object[]{0, 100, 200});
+
+        row.put("a", 300);
+        row.remove("b");
+        batch.add(100, row);
+        want.add(new Object[]{100, 300, null});
+
+        JSONObject json = batch.toJson();
+
+        assertTrue(json.has("fields"));
+        JSONArray jsonFields = json.getJSONArray("fields");
+        // "time" field + those passed in = 1 + fields.length
+        assertEquals(1 + fields.size(), jsonFields.length());
+        assertEquals("time", jsonFields.get(0));
+        for (int i = 0; i < fields.size(); i++) {
+            assertTrue(fields.contains(jsonFields.get(i + 1)));
+        }
+
+        assertTrue(json.has("data"));
+        JSONArray data = json.getJSONArray("data");
+        assertEquals(want.size(), data.length());
+        for (int i = 0; i < data.length(); i++) {
+            JSONArray r = data.getJSONArray(i);
+            Object[] w = want.get(i);
+            assertEquals(w.length, r.length());
+            for (int j = 0; j < w.length; j++) {
+                // The JSON lib assumes that a 'null' return value for a JSONArray.get() call means
+                // it is an index out of bounds. So we have to do this weird hack to handle nulls
+                // correctly (we already know the arrays are the same size.
+                Integer have;
+                try {
+                    have = r.getInt(j);
+                } catch (JSONException e) {
+                    if (w[j] == null) {
+                        assertTrue(true);
+                    } else {
+                        assertTrue(false);
+                    }
+                    continue;
+                }
+                if (w[j] == null) {
+                    assertTrue(false);
+                } else {
+                    assertEquals(Integer.valueOf(w[j].toString()).intValue(), have.intValue());
+                }
+            }
+        }
+    }
+
+}
